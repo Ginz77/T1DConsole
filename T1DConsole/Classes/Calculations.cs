867cdfb61ba9b14596ed59f5bb4c141b6115f6ed
Phil Ginsburg <ginz1977@gmail.com>
2022-02-21 19:37:12 -0500

    Commit files

---
+++ T1DConsole/Classes/Calculations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace T1DConsole.Classes
+{
+	public class Calculations
+	{
+		// Values
+		public float Carb { get; set; }
+		public float CarbRatio { get; set; }
+		public float BloodSugar { get; set; }
+		public float TargetBloodSugar { get; set; }
+		public float CorrectionFactor { get; set; }
+
+
+		// Carbs formula
+		public float CalculateCarbs(float carb, float carbRatio)
+        {
+			float result = carb / carbRatio;
+			return result;
+        }
+
+		// Blood sugar formula
+		public float CalculateBloodSugar(float bloodSugar, float targetBloodSugar, float correctionFactor)
+		{
+			float result = (bloodSugar - targetBloodSugar) / correctionFactor;
+			return result;
+		}
+	}
+}
+
---
+++ T1DConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using T1DConsole.Classes;
+
+float carbRatioVal = 22;
+float TargetBloodSugarVal = 120;
+float CorrectionFactorVal = 80;
+
+Console.WriteLine("Enter Carbs:");
+
+Calculations calcObj = new Calculations();
+
+// Carbs to be entered;
+calcObj.Carb = float.Parse(Console.ReadLine()); 
+calcObj.CarbRatio = carbRatioVal;
+Console.WriteLine("----------------------");
+Console.WriteLine($"Carb calculation: {calcObj.CalculateCarbs(calcObj.Carb, calcObj.CarbRatio).ToString("n2")}");
+Console.WriteLine("----------------------");
+
+Console.WriteLine("Enter Blood Sugar:");
+
+// Blood sugar reading to be entered
+calcObj.BloodSugar = float.Parse(Console.ReadLine());
+calcObj.TargetBloodSugar = TargetBloodSugarVal;
+calcObj.CorrectionFactor = CorrectionFactorVal;
+Console.WriteLine("----------------------");
+Console.WriteLine($"Blood Sugar: {calcObj.CalculateBloodSugar(calcObj.BloodSugar, calcObj.TargetBloodSugar, calcObj.CorrectionFactor).ToString("n2")}");
+Console.WriteLine("----------------------");
+
+// Total units of insulin
+Console.WriteLine("**********************");
+Console.WriteLine($"Total Units: {(calcObj.CalculateCarbs(calcObj.Carb, calcObj.CarbRatio) + calcObj.CalculateBloodSugar(calcObj.BloodSugar, calcObj.TargetBloodSugar, calcObj.CorrectionFactor)).ToString("n2")}");
+Console.WriteLine("**********************");
+
